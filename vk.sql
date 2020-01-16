b999b329e889598da4cf60969cbdd33eae910748
Dann38 <38dunn@gmail.com>
2020-01-16 20:35:15 +0800

    Сделал 2 задания, но оказалось это нужно было сделать не для этой базы данных

--- vk.sql
+++ vk.sql
@@ -360,9 +360,15 @@ INSERT INTO `photos` VALUES ('1','1','1'),
 2)Таблица users была неудачно спроектирована. Записи created_at и updated_at были заданы типом VARCHAR и в них долгое время помещались значения в формате "20.10.2017 8:10". Необходимо преобразовать поля к типу DATETIME, сохранив введеные ранее значения.
 3)В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0, если товар закончился и выше нуля, если на складе имеются запасы. Необходимо отсортировать записи таким образом, чтобы они выводились в порядке увеличения значения value. Однако, нулевые запасы должны выводиться в конце, после всех записей.
 */
+ALTER TABLE users ADD COLUMN created_at VARCHAR(60);
+ALTER TABLE users ADD COLUMN updated_at VARCHAR(60);
+UPDATE users SET created_at=NOW() WHERE created_at IS NULL;
+UPDATE users SET updated_at=NOW() WHERE updated_at IS NULL;
 
+ALTER TABLE users CHANGE created_at created_at DATETIME;
+ALTER TABLE users CHANGE updated_at updated_at DATETIME;
 
-
+USE vk;
 
 
 
